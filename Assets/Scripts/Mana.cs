c9c1f874f0748bcb2c3a4a4714f539759c27fb4a
David Silva <herytic@gmail.com>
2024-04-14 12:30:58 +0200

    auto mana collection

--- Assets/Scripts/Mana.cs
+++ Assets/Scripts/Mana.cs
@@ -1,15 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Mana : MonoBehaviour, IPointerDownHandler
+public class Mana : MonoBehaviour
 {
     public int manaAmount;
+    public RectTransform target;
 
-    public void OnPointerDown(PointerEventData eventData)
+    void Start()
     {
+        StartMovement();
+    }
+
+    public Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = uu * p0;
+        p += 2 * u * t * p1;
+        p += tt * p2;
+        return p;
+    }
+
+    IEnumerator MoveObjectAlongCurve(GameObject obj, Vector3 start, Vector3 controlPoint, Vector3 end, float duration)
+    {
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            Vector3 position = CalculateBezierPoint(t, start, controlPoint, end);
+            obj.transform.position = position;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        obj.transform.position = end;
+
         ManaManager.instance.manaAmount += manaAmount;
         Destroy(gameObject);
+    }
+
+    void StartMovement()
+    {
+        Vector3 startPoint = gameObject.transform.position;
+        Vector3 endPoint = target.transform.position;
+        int randomSign = Random.value < 0.5 ? -1 : 1;
+        Vector3 controlPoint = (startPoint + endPoint) / 2 + new Vector3(200 * randomSign, 200, 0);
+
+        StartCoroutine(MoveObjectAlongCurve(gameObject, startPoint, controlPoint, endPoint, 0.2f));
     }
 }
--- Assets/Scripts/ManaManager.cs
+++ Assets/Scripts/ManaManager.cs
@@ -6,13 +6,12 @@ using UnityEngine.UI;
 public class ManaManager : MonoBehaviour
 {
     public Vector2 randomSpawnDelaySeconds;
-    public GameObject[] spawnPlaces;
     public GameObject spawnPlace;
+    public GameObject summoner;
     public GameObject manaObject;
     public Text manaAmountText;
     public int manaAmount = 0;
-
-    private float nextSpawnTime;
+    public RectTransform ui;
 
     public static ManaManager instance;
 
@@ -24,43 +23,22 @@ public class ManaManager : MonoBehaviour
 
     IEnumerator Start()
     {
-        SpawnEnergyObjectRandom();
-        yield return new WaitForSeconds(3);
-        SpawnEnergyObjectRandom();
+        yield return new WaitForSeconds(1);
+        SpawnEnergyObject(summoner.transform.position);
+        yield return new WaitForSeconds(1);
+        SpawnEnergyObject(summoner.transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
         manaAmountText.text = manaAmount.ToString();
-        //if (Time.time >= nextSpawnTime)
-        //{
-        //    //SpawnEnergyObject();
-        //    //SetNextSpawnTime();
-        //}
-    }
-
-    void SetNextSpawnTime()
-    {
-        nextSpawnTime = Time.time + Random.Range(randomSpawnDelaySeconds.x, randomSpawnDelaySeconds.y);
-    }
-
-    public void SpawnEnergyObjectRandom()
-    {
-        if (spawnPlaces.Length > 0)
-        {
-            int randomIndex = Random.Range(0, spawnPlaces.Length);
-            if (spawnPlaces[randomIndex] != null)
-            {
-                Instantiate(manaObject, spawnPlaces[randomIndex].transform).transform.position = spawnPlaces[randomIndex].transform.position;
-            }
-        }
     }
 
     public void SpawnEnergyObject(Vector3 position)
     {
         GameObject obj = Instantiate(manaObject, spawnPlace.transform);
         obj.transform.position = position;
-           
+        obj.GetComponent<Mana>().target = ui;
+
     }
 }
